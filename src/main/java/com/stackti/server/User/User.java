4342c2175cdc4f953ee4b76a1def058f599300d1
Felipe Guimarães <54296382+felipe2g@users.noreply.github.com>
2022-11-17 19:38:48 -0300

    add initial to crud user

---
+++ src/main/java/com/stackti/server/User/User.java
@@ -0,0 +1,19 @@
+package com.stackti.server.User;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    public String firstName;
+    public String lastName;
+    public String email;
+    public String password;
+    public int role;
+    public String jobTitle;
+    public int points;
+    public int reputation;
+}
---
+++ src/main/java/com/stackti/server/User/UserController.java
@@ -0,0 +1,14 @@
+package com.stackti.server.User;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UserController {
+    @GetMapping("/sign-up")
+    public String getSignUp(Model model) {
+        model.addAttribute("user", new User());
+        return "sign-up";
+    }
+}
