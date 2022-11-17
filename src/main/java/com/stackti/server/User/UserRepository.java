023b5ddbaa1a0c7ff5d2807c60afd7ac9807d1d1
Felipe Guimarães <54296382+felipe2g@users.noreply.github.com>
2022-11-17 20:33:50 -0300

    add jdbc to project

---
+++ src/main/java/com/stackti/server/User/UserRepository.java
@@ -0,0 +1,7 @@
+package com.stackti.server.User;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+public class UserRepository {
+}
